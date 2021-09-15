c20a05de622e4c2a243b63f58cdf87a18de5b83a
Christopher Lyon <christopherlyon.cl@gmail.com>
2021-09-15 21:49:01 +0200

    Added PDF generator
    + Bug fixes

--- python/barcode-generator.py
+++ python/barcode-generator.py
@@ -9,20 +9,39 @@ name = input("Name of music: ")
 index = input("Starting number: ")
 to_index = input("Last number in series: ")
 pdf = FPDF()
-output = "generated/"
-generated = os.listdir(output)
+outputdir = "temp/"
 
 while int(index) <= int(to_index):
 
-    with open("generated/" + str(name) + str(index) + '.jpeg', 'wb') as f:
+    with open(outputdir + str(name) + str(index) + '.jpeg', 'wb') as f:
         Code128((str(name) + " " + str(index)), writer=ImageWriter()).write(f)
     index = int(index) + 1
 
-time.sleep(5)
 
-print("check 1")
-# This would print all the files and directories
-for image in generated:
-    pdf.add_page()
-    pdf.image(image, 0, 0, 210, 297)
-pdf.output("yourfile.pdf", "F")
+pdf.alias_nb_pages()
+pdf.add_page()
+pdf.set_font('Times', '', 12)
+time.sleep(1)
+
+generated = os.listdir(outputdir)
+
+
+y = 5
+x = 5
+col = 0
+
+for image in sorted(generated):
+    print(image)
+    pdf.image(outputdir + image, x, y, 50, 35)
+    x = x + 70
+    if x > 145:
+        x = 5
+        y = y + 40
+
+
+# for i in range(1, 10):
+#    pdf.cell(0, 10, 'Printing line number ' + str(i), 0, 1)
+pdf.output("generated-pdf/" + name + ".pdf", "F")
+
+for f in os.listdir(outputdir):
+    os.remove(os.path.join(outputdir, f))
